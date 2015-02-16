4f0423fd6dddf8e784e4a29c3637bee33845d8cd
Stephen Phillips <stephi@LVN403-2.CIS.upenn.edu>
2015-02-16 13:24:55 -0500

    Finished the next problem in MEAM620 HW

--- HW2/scratch.m
+++ HW2/scratch.m
@@ -1,3 +1,4 @@
+%% Cubic spline
 % Create constants for the spline
 X    = [ -1;   0;   1 ];
 Y    = [  0;   2;   0 ];
@@ -59,7 +60,7 @@ hold on;
 plot(xt1,yt1)
 plot(xt2,yt2)
 
-%%
+%% Septic spline
 
 % Create constants for the spline
 X    = [ -1;   0;   1 ];
@@ -93,56 +94,70 @@ by(4)      =  0;
 
 % Time 2 constraints
 % Final position for first segment
-A(4,seg1)  =  [T(2)^7, T(2)^6, T(2)^5, T(2)^4, T(2)^3, T(2)^2, T(2), 1];
-bx(4)      =  X(2);
-by(4)      =  Y(2);
-% Initial position for second
-A(5,seg2)  =  [T(2)^7, T(2)^6, T(2)^5, T(2)^4, T(2)^3, T(2)^2, T(2), 1];
+A(5,seg1)  =  [T(2)^7, T(2)^6, T(2)^5, T(2)^4, T(2)^3, T(2)^2, T(2), 1];
 bx(5)      =  X(2);
 by(5)      =  Y(2);
+% Initial position for second
+A(6,seg2)  =  [T(2)^7, T(2)^6, T(2)^5, T(2)^4, T(2)^3, T(2)^2, T(2), 1];
+bx(6)      =  X(2);
+by(6)      =  Y(2);
 % Velocities must be the same for both segments
-A(6,seg1)  =  [7*T(2)^6, 6*T(2)^5, 5*T(2)^4 4*T(2)^3 3*T(2)^2 2*T(2) 1 0];
-A(6,seg2)  = -[7*T(2)^6, 6*T(2)^5, 5*T(2)^4 4*T(2)^3 3*T(2)^2 2*T(2) 1 0];
-bx(6)      =  0;
-by(6)      =  0;
-% Accelerations must be the same for both segments
-A(7,seg1)  =  [42*T(2)^5, 30*T(2)^4, 20*T(2)^3 12*T(2)^2 6*T(2) 2 0 0];
-A(7,seg2)  = -[42*T(2)^5, 30*T(2)^4, 20*T(2)^3 12*T(2)^2 6*T(2) 2 0 0];
+A(7,seg1)  =  [7*T(2)^6, 6*T(2)^5, 5*T(2)^4 4*T(2)^3 3*T(2)^2 2*T(2) 1 0];
+A(7,seg2)  = -[7*T(2)^6, 6*T(2)^5, 5*T(2)^4 4*T(2)^3 3*T(2)^2 2*T(2) 1 0];
 bx(7)      =  0;
 by(7)      =  0;
-% Jerk must be the same for both segments
-A(8,seg1)  =  [210*T(2)^4, 120*T(2)^3, 60*T(2)^2 24*T(2) 6 0 0 0];
-A(8,seg2)  = -[210*T(2)^4, 120*T(2)^3, 60*T(2)^2 24*T(2) 6 0 0 0];
+% Accelerations must be the same for both segments
+A(8,seg1)  =  [42*T(2)^5, 30*T(2)^4, 20*T(2)^3 12*T(2)^2 6*T(2) 2 0 0];
+A(8,seg2)  = -[42*T(2)^5, 30*T(2)^4, 20*T(2)^3 12*T(2)^2 6*T(2) 2 0 0];
 bx(8)      =  0;
 by(8)      =  0;
-% Snap must be the same for both segments
-A(9,seg1)  =  [630*T(2)^3, 360*T(2)^2, 120*T(2) 24 0 0 0 0];
-A(9,seg2)  = -[630*T(2)^3, 360*T(2)^2, 120*T(2) 24 0 0 0 0];
+% Jerk must be the same for both segments
+A(9,seg1)  =  [210*T(2)^4, 120*T(2)^3, 60*T(2)^2 24*T(2) 6 0 0 0];
+A(9,seg2)  = -[210*T(2)^4, 120*T(2)^3, 60*T(2)^2 24*T(2) 6 0 0 0];
 bx(9)      =  0;
 by(9)      =  0;
-
-% Time 3 constraints
-% Final position for first segment
-A(10,seg2) =  [T(3)^5, T(3)^4, T(3)^3, T(3)^2, T(3), 1];
-bx(10)     =  X(3);
-by(10)     =  Y(3);
-% Velocity vanishes at the end
-A(11,seg2) =  [5*T(3)^4 4*T(3)^3 3*T(3)^2 2*T(3) 1 0];
+% 'Snap' must be the same for both segments
+A(10,seg1) =  [630*T(2)^3, 360*T(2)^2, 120*T(2), 24, 0, 0, 0, 0];
+A(10,seg2) = -[630*T(2)^3, 360*T(2)^2, 120*T(2), 24, 0, 0, 0, 0];
+bx(10)     =  0;
+by(10)     =  0;
+% 'Crackle' must be the same for both segments
+A(11,seg1) =  [1890*T(2)^2, 720*T(2), 120, 0, 0, 0, 0, 0];
+A(11,seg2) = -[1890*T(2)^2, 720*T(2), 120, 0, 0, 0, 0, 0];
 bx(11)     =  0;
 by(11)     =  0;
-% Final acceleration
-A(12,seg2) =  [20*T(3)^3 12*T(3)^2 6*T(3) 2 0 0];
+% 'Pop' must be the same for both segments
+A(12,seg1) =  [3780*T(2), 720, 0, 0, 0, 0, 0, 0];
+A(12,seg2) = -[3780*T(2), 720, 0, 0, 0, 0, 0, 0];
 bx(12)     =  0;
 by(12)     =  0;
 
 
+% Time 3 constraints
+% Final position for first segment
+A(13,seg2) =  [T(3)^7, T(3)^6, T(3)^5, T(3)^4, T(3)^3, T(3)^2, T(3), 1];
+bx(13)     =  X(3);
+by(13)     =  Y(3);
+% Velocity vanishes at the end
+A(14,seg2) =  [7*T(3)^6, 6*T(3)^5, 5*T(3)^4, 4*T(3)^3, 3*T(3)^2, 2*T(3), 1, 0];
+bx(14)     =  0;
+by(14)     =  0;
+% Final acceleration
+A(15,seg2) =  [42*T(3)^5, 30*T(3)^4, 20*T(3)^3, 12*T(3)^2, 6*T(3), 2, 0, 0];
+bx(15)     =  0;
+by(15)     =  0;
+% Final jerk
+A(16,seg1)  =  [210*T(3)^4, 120*T(3)^3, 60*T(3)^2 24*T(2) 6 0 0 0];
+bx(16)      =  0;
+by(16)      =  0;
+
 % Compute the coefficents
 Cx = A \ bx;
 Cy = A \ by;
-fprintf('Coefficients for x, segment 1: (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cx(1),Cx(2),Cx(3),Cx(4),Cx(5),Cx(6));
-fprintf('Coefficients for y, segment 1: (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cy(1),Cy(2),Cy(3),Cy(4),Cy(5),Cy(6));
-fprintf('Coefficients for x, segment 2: (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cx(7),Cx(8),Cx(9),Cx(10),Cx(11),Cx(12));
-fprintf('Coefficients for y, segment 2: (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cy(7),Cy(8),Cy(9),Cy(10),Cy(11),Cy(12));
+fprintf('Coefficients for x, segment 1:\n (%0.4f)*t^7 + (%0.4f)*t^6 + (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cx(1),Cx(2),Cx(3),Cx(4),Cx(5),Cx(6),Cx(7),Cx(8));
+fprintf('Coefficients for y, segment 1:\n (%0.4f)*t^7 + (%0.4f)*t^6 + (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cy(1),Cy(2),Cy(3),Cy(4),Cy(5),Cy(6),Cy(7),Cy(8));
+fprintf('Coefficients for x, segment 2:\n (%0.4f)*t^7 + (%0.4f)*t^6 + (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cx(9),Cx(10),Cx(11),Cx(12),Cx(13),Cx(14),Cx(15),Cx(16));
+fprintf('Coefficients for y, segment 2:\n (%0.4f)*t^7 + (%0.4f)*t^6 + (%0.4f)*t^5 + (%0.4f)*t^4 + (%0.4f)*t^3 + (%0.4f)*t^2 + (%0.4f)*t + (%0.4f)\n',Cy(9),Cy(10),Cy(11),Cy(12),Cy(13),Cy(14),Cy(15),Cy(16));
 
 scatter(X,Y)
 t1 = (T(1):0.1:T(2))';
@@ -155,7 +170,7 @@ hold on;
 plot(xt1,yt1)
 plot(xt2,yt2)
 
-%%
+%% Quintic Spline
 
 % Create constants for the spline
 X    = [ -1;   0;   1 ];
@@ -238,11 +253,11 @@ fprintf('Coefficients for y, segment 2: (%f)*t^5 + (%f)*t^4 + (%f)*t^3 + (%f)*t^
 
 scatter(X,Y)
 t1 = (T(1):0.1:T(2))';
-xt1 = [ t1.^7 t1.^6 t1.^5 t1.^4 t1.^3 t1.^2 t1.^1 ones(size(t1)) ]*Cx(1:6);
-yt1 = [ t1.^7 t1.^6 t1.^5 t1.^4 t1.^3 t1.^2 t1.^1 ones(size(t1)) ]*Cy(1:6);
+xt1 = [ t1.^5 t1.^4 t1.^3 t1.^2 t1.^1 ones(size(t1)) ]*Cx(1:6);
+yt1 = [ t1.^5 t1.^4 t1.^3 t1.^2 t1.^1 ones(size(t1)) ]*Cy(1:6);
 t2 = (T(2):0.1:T(3))';
-xt2 = [ t2.^7 t2.^6 t2.^5 t2.^4 t2.^3 t2.^2 t2.^1 ones(size(t2)) ]*Cx(7:12);
-yt2 = [ t2.^7 t2.^6 t2.^5 t2.^4 t2.^3 t2.^2 t2.^1 ones(size(t2)) ]*Cy(7:12);
+xt2 = [ t2.^5 t2.^4 t2.^3 t2.^2 t2.^1 ones(size(t2)) ]*Cx(7:12);
+yt2 = [ t2.^5 t2.^4 t2.^3 t2.^2 t2.^1 ones(size(t2)) ]*Cy(7:12);
 hold on;
 plot(xt1,yt1)
 plot(xt2,yt2)
