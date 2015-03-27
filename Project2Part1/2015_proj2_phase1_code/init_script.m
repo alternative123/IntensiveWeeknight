9989ae8065c9b1507ff7a5270914c9a4bf5d4e39
Stephen Phillips <chengdu.scp@gmail.com>
2015-03-27 17:13:14 -0400

    Fixed a small bug

--- Project2Part1/2015_proj2_phase1_code/init_script.m
+++ Project2Part1/2015_proj2_phase1_code/init_script.m
@@ -7,17 +7,6 @@
 % handle, but not run the function at all.
 
 estimate_pose_handle = @(sensor) estimate_pose(sensor);
-
-
-%%
-pts = 1:12;
-scatter(d.p0(1,pts),d.p0(2,pts),'.'); hold on; 
-scatter(d.p1(1,pts),d.p1(2,pts)); hold on; 
-scatter(d.p2(1,pts),d.p2(2,pts)); hold on; 
-scatter(d.p3(1,pts),d.p3(2,pts)); hold on;
-scatter(d.p4(1,pts),d.p4(2,pts)); hold on;
-
-%%
-     
+  
 
 
