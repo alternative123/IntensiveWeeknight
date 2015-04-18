fbca5911944d2ef3397f0f5a2e6e515ca1bbe6fc
Stephen Phillips <chengdu.scp@gmail.com>
2015-04-17 21:46:52 -0400

    Fixed a little thing

--- Project2Part3/studentcode/estimate_pose.m
+++ Project2Part3/studentcode/estimate_pose.m
@@ -30,6 +30,15 @@ if any(size(sensor.id) == 0)
     return
 end
 
+% Camera-IMU Calibration (see attached images for details):
+XYZ = [-0.04, 0.0, -0.03]';
+Yaw = -pi/4;
+Roll = pi;
+Rz = @(th) [cos(th), -sin(th), 0; sin(th), cos(th), 0; 0, 0, 1];
+Rx = @(th) [1, 0, 0; 0, cos(th), -sin(th); 0, sin(th), cos(th)];
+R_toIMU = Rz(Yaw)*Rx(Roll);
+T_toIMU = -R_toIMU'*XYZ;
+Trans_toIMU = [ R_toIMU', T_toIMU; 0 0 0 1 ];
 w_tag = 0.152; % Tag width
 
 ids = sensor.id+1; % April tag ids seen in this image
