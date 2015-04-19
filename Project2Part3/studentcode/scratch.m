e09e89f9bb92a2e0e3aa8dc7e8c9d1379a19f8fd
Stephen Phillips <chengdu.scp@gmail.com>
2015-04-18 22:50:20 -0400

    Fixed the typo with the Rotation matrix

--- Project2Part3/studentcode/scratch.m
+++ Project2Part3/studentcode/scratch.m
@@ -18,31 +18,62 @@
 % Jnoise = jacobian(xdot,n);
 % 
 %% EKF 2
-% 
-% clear
-% 
-% % Create the symbols and functions we need
-% x = sym('x',[15,1]); % x = [p; q; pdot; b_g; b_a];
-% w_m = sym('w_m',[3,1]); % Measured angular velocity
-% a_m = sym('a_m',[3,1]); % Measured acceleration
-% n = sym('n',[15,1]); % Gyroscope noise
-% 
-% R = @(q) [ cos(q(3))*cos(q(2))-sin(q(1))*sin(q(3))*cos(q(2)), -cos(q(1))*sin(q(3)), cos(q(3))*sin(q(2))-cos(q(2))*sin(q(1))*cos(q(3));
-%            cos(q(2))*sin(q(3))+cos(q(3))*sin(q(1))*sin(q(2)), cos(q(1))*cos(q(3)), sin(q(3))*sin(q(2))-cos(q(3))*cos(q(2))*sin(q(1));
-%            -cos(q(1))*sin(q(2)), sin(q(1)), cos(q(1))*cos(q(2)) ];
-% G = @(q) [cos(q(2)) 0 -cos(q(1))*sin(q(2));
-%               0     1         0;
-%           sin(q(2)) 0  cos(q(1))*cos(q(2)) ];
-% 
-% xdot = [ x(7:9) + n(1:3);
-%          inv(G(x(4:6)))*(w_m - x(10:12) - n(4:6));
-%          [0,0,-9.81]'+R(x(4:6))*(a_m - x(13:15) - n(7:9));
-%          n(10:12);
-%          n(13:15) ];
-% 
-% J = jacobian(xdot,x);
-% Jnoise = jacobian(xdot,n);
-     
+
+clear
+
+% Create the symbols and functions we need
+x = sym('x',[15,1]); % x = [p; q; pdot; b_g; b_a];
+w_m = sym('w_m',[3,1]); % Measured angular velocity
+a_m = sym('a_m',[3,1]); % Measured acceleration
+n = sym('n',[15,1]); % Gyroscope noise
+
+R = @(q) [cos(q(3))*cos(q(2)) - sin(q(1))*sin(q(3))*sin(q(2)), ...
+     cos(q(2))*sin(q(3)) + cos(q(3))*sin(q(1))*sin(q(2)), ...
+     -cos(q(1))*sin(q(2)); ...
+     -cos(q(1))*sin(q(3)),...
+     cos(q(1))*cos(q(3)), ...
+     sin(q(1));...
+     cos(q(3))*sin(q(2)) + cos(q(2))*sin(q(1))*sin(q(3)),...
+     sin(q(3))*sin(q(2)) - cos(q(3))*cos(q(2))*sin(q(1)),...
+     cos(q(1))*cos(q(2))];
+
+G = @(q) [cos(q(2)) 0 -cos(q(1))*sin(q(2));
+              0     1         0;
+          sin(q(2)) 0  cos(q(1))*cos(q(2)) ];
+
+xdot = [ x(7:9) + n(1:3);
+         inv(G(x(4:6)))*(w_m - x(10:12) - n(4:6));
+         [0,0,-9.81]'+R(x(4:6))*(a_m - x(13:15) - n(7:9));
+         n(10:12);
+         n(13:15) ];
+
+J = jacobian(xdot,x);
+Jnoise = jacobian(xdot,n);
+
+%%
+q = sym('q',[3,1]);
+Rz = @(z) [cos(z), -sin(z), 0;
+           sin(z),  cos(z), 0;
+             0   ,    0   , 1 ];
+Rx = @(x) [ 1,   0   ,   0    ;
+            0, cos(x), -sin(x);
+            0, sin(x),  cos(x) ];
+Ry = @(y) [  cos(y), 0, sin(y);
+               0,    1,   0   ;
+            -sin(y), 0, cos(y) ];
+
+R = @(q) [cos(q(3))*cos(q(2)) - sin(q(1))*sin(q(3))*sin(q(2)), ...
+     cos(q(2))*sin(q(3)) + cos(q(3))*sin(q(1))*sin(q(2)), ...
+     -cos(q(1))*sin(q(2)); ...
+     -cos(q(1))*sin(q(3)),...
+     cos(q(1))*cos(q(3)), ...
+     sin(q(1));...
+     cos(q(3))*sin(q(2)) + cos(q(2))*sin(q(1))*sin(q(3)),...
+     sin(q(3))*sin(q(2)) - cos(q(3))*cos(q(2))*sin(q(1)),...
+     cos(q(1))*cos(q(2))];
+disp(simplify(R(q)*transpose(R(q)) == eye(3)))
+
+
 
 %%
 
@@ -64,19 +95,19 @@
 % ekf1_handle(sensor, vic)
 
 %%
-
-clear
-clear ekf2
-init_script
-
-load('/home/stephen/Dropbox/Documents/PhD/Classes/MEAM620/Project2Part3/studentcode/data/studentdata1.mat')
-sensor = data(1);
-
-ekf2_handle(sensor)
-
-sensor = data(2);
-
-ekf2_handle(sensor)
+% 
+% clear
+% clear ekf2
+% init_script
+% 
+% load('/home/stephen/Dropbox/Documents/PhD/Classes/MEAM620/Project2Part3/studentcode/data/studentdata1.mat')
+% sensor = data(1);
+% 
+% ekf2_handle(sensor)
+% 
+% sensor = data(2);
+% 
+% ekf2_handle(sensor)
 
 
 
